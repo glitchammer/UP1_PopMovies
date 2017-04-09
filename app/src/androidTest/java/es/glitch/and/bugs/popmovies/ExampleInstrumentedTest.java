f5651daca286c14fac550b9953ccc53d1cc74ff6
glitchammer <dnlbhls@gmail.com>
2017-04-09 09:11:19 +0200

    .... and the rest of it (refactored package and app name to es.glitch.and.bugs.popmovies)

--- app/src/androidTest/java/es/glitch/and/bugs/popmovies/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/es/glitch/and/bugs/popmovies/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.android.up1_popmovies", appContext.getPackageName());
+        assertEquals("es.glitch.and.bugs.popmovies", appContext.getPackageName());
     }
 }
