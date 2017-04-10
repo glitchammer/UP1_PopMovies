f81fe615fc2c8f18a5f865ee66d90a6b72f2311b
glitchammer <dnlbhls@gmail.com>
2017-04-10 15:03:04 +0200

    add content provider (unfinished)

---
+++ app/src/main/java/es/glitch/and/bugs/popmovies/data/MovieContentProvider.java
@@ -0,0 +1,70 @@
+package es.glitch.and.bugs.popmovies.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class MovieContentProvider extends ContentProvider {
+
+
+    public static final int MOVIES         = 100;
+    public static final int MOVIES_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES + "/#", MOVIES_WITH_ID);
+        return uriMatcher;
+    }
+
+    private MovieDBHelper movieDBHelper;
+
+    public MovieContentProvider() {
+    }
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        movieDBHelper = new MovieDBHelper(context);
+        return true;
+    }
+
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO: Implement this to handle requests to insert a new row.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
--- app/src/main/java/es/glitch/and/bugs/popmovies/data/MovieContract.java
+++ app/src/main/java/es/glitch/and/bugs/popmovies/data/MovieContract.java
@@ -1,5 +1,6 @@
 package es.glitch.and.bugs.popmovies.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,7 +9,16 @@ import android.provider.BaseColumns;
 
 public class MovieContract {
 
+    public static final String AUTHORITY = "es.glitch.and.bugs.popmovies";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final String PATH_MOVIES = "movies";
+
     public static final class MoviesEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
 
         public static final String TABLE_NAME           = "movies";
 
